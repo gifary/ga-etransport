c8a9c43ae053ef6b05ef381ba2801157b712672f
Firman Firdaus <firmanfirdaus17.jtk12@gmail.com>
2023-12-18 16:10:42 +0700

    Fix model OrganizationLocation

--- src/Models/OrganizationLocation.php
+++ src/Models/OrganizationLocation.php
@@ -8,6 +8,26 @@ class OrganizationLocation extends BaseModelETransport
 {
 
     protected $table = 'public.m_organization_location';
-    protected $guard = ['id'];
+    protected $fillable = 
+        [
+            'id',
+            'sap_organization_id',
+            'latitude',
+            'longitude',
+            'timezone',
+            'created_by',
+            'updated_by',
+            'created_at',
+            'updated_at',
+            'deleted_at',
+            'personel_area',
+            'personel_sub_area',
+            'parent',
+            'name',
+            'personel_area_name',
+            'personel_sub_area_name',
+            'divisi_id',
+            'range_allowed'
+        ];
 
 }
